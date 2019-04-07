7a32cc1857eded624c5a57fc92aaacb423b90821
SylarLi <328403302@qq.com>
2019-04-07 19:10:12 +0800

    Add icon alignment.

---
+++ Assets/RichText/Demo/TestRichText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestRichText : MonoBehaviour
+{
+    void Start()
+    {
+        var richText = GetComponent<RichText>();
+        richText.AddListener((e, args) =>
+        {
+            Debug.Log("click: " + e + ", args: " + args);
+        });
+    }
+}
